016d043a53220472bae76398399e562c7fc3cf65
ZarakiLancelot <kenpachi2783@gmail.com>
2023-10-14 20:06:35 -0600

    formularios de registro e inicio de sesión

--- trivia/forms.py
+++ trivia/forms.py
@@ -1,11 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email
+from wtforms import StringField, PasswordField, SubmitField, EmailField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegisterForm(FlaskForm):
     username = StringField(label='username')
-    email_address = StringField(label='email')
-    password1 = PasswordField(label='password')
-    password2 = PasswordField(label='repeat password')
+    nombre = StringField(label="nombre")
+    email = EmailField(label='email')
+    password = PasswordField(label='password', validators=[DataRequired(),
+                                                           EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField(label='confirm password', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
     submit = SubmitField(label='submit')
