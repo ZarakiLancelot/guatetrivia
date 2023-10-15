1dc72cd5593d993757d93432190329c991578ff3
ZarakiLancelot <kenpachi2783@gmail.com>
2023-10-15 07:27:55 -0600

    registro implementado

--- trivia/__init__.py
+++ trivia/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///guatetrivia.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'df46a34613411bda580e017b'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 
 def create_db():
--- trivia/forms.py
+++ trivia/forms.py
@@ -1,14 +1,38 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, EmailField, SelectField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, EmailField, SelectField, DateField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from trivia.models import User
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    nombre = StringField(label="nombre")
-    email = EmailField(label='email')
-    password = PasswordField(label='password', validators=[DataRequired(),
-                                                           EqualTo('confirm_password', message='Passwords must match')])
-    confirm_password = PasswordField(label='confirm password', validators=[DataRequired()])
-    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
-    submit = SubmitField(label='submit')
+    @staticmethod
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('El nombre de usuario ya existe, por favor intenta con otro nombre de usuario')
+
+    @staticmethod
+    def validate_email(self, email_to_check):
+        email = User.query.filter_by(email=email_to_check.data).first()
+        if email:
+            raise ValidationError('El correo electrónico ya existe, por favor intenta con otro correo electrónico')
+
+    username = StringField(label='Usuario', validators=[Length(min=5, max=20), DataRequired()])
+    nombre = StringField(label="Nombre", validators=[DataRequired()])
+    email = EmailField(label='Correo Electrónico', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Contraseña',
+                             validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField(label='Confirmar contraseña',
+                                     validators=[DataRequired(),
+                                                 EqualTo('password', message='Las contraseñas deben de ser iguales')
+                                                 ])
+    genero = SelectField('Género', choices=[('masculino', 'Masculino'), ('femenino', 'Femenino')],
+                         validators=[DataRequired()])
+    fecha_nacimiento = DateField(label="Fecha de Nacimiento")
+    submit = SubmitField(label='Crear Cuenta')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label="Usuario", validators=[DataRequired()])
+    password = PasswordField(label="Contraseña", validators=[DataRequired()])
+    submit = SubmitField(label="Iniciar Sesión")
